4a0b5979a4112182d11824eda009e43835c5939b
eflisback <ebbe.flisback@gmail.com>
2023-08-31 13:40:51 +0200

    Good script

---
+++ EDAA45/V35/code/typesAndValues.scala
@@ -0,0 +1,8 @@
+// Use this script in order to test different expressions in Scala and see their resulting values/types
+@main def typesAndValues = {
+  val testValue = 'A' + '0' // Expression
+
+  // Printing
+  println("Value is: " + testValue)
+  println("Type is: " + testValue.getClass())
+}
